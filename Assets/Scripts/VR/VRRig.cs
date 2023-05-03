52840a3e4649154d42c7084c33bf5bc3206a0941
Uderr <ulderico.vagnoni@studio.unibo.it>
2023-05-03 16:55:42 +0200

    Valem part 2 legs working

--- Assets/Scripts/VR/VRRig.cs
+++ Assets/Scripts/VR/VRRig.cs
@@ -9,12 +9,20 @@ public class VRMap
 {
     public Transform vrTarget;
     public Transform rigTarget;
+    public float initHeadY;
     public Vector3 trackingPositionOffset;
     public Vector3 trackingRotationOffset;
 
     public void Map()
     {
         rigTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+    public void MapHead()
+    {
+        initHeadY = rigTarget.transform.position.y;
+        rigTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        rigTarget.position = new Vector3(rigTarget.position.x, initHeadY, rigTarget.position.z);
         rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
     }
 
@@ -83,7 +91,7 @@ public class VRRig : MonoBehaviour
 
         //spine.transform.forward = Vector3.Lerp(spine.transform.forward, Vector3.ProjectOnPlane(newMiddle, Vector3.up).normalized, Time.deltaTime * smoothness);
 
-        head.Map();
+        head.MapHead();
         leftHand.Map();
         rightHand.Map();
 
